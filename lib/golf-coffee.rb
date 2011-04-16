d284d992ccf4264e5ff75951b4a415c7fc68f61e
Julio Capote <jcapote@gmail.com>
2011-04-16 12:12:36 -0700

    no_wrap true by default

--- lib/golf-coffee.rb
+++ lib/golf-coffee.rb
@@ -3,12 +3,12 @@ module Golf
   module Filter
     class Coffee
       def self.transform(data)
-        CoffeeScript.compile(data)
+        CoffeeScript.compile(data, :no_wrap => true)
       end
     end
     class Cs
       def self.transform(data)
-        CoffeeScript.compile(data)
+        CoffeeScript.compile(data, :no_wrap => true)
       end
     end
   end
